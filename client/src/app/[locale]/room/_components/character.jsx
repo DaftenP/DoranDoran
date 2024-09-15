a8c53f7dd28eee914ddeb8eec5664b65bfd7b9b5
kdhn97 <ehdgus3726@naver.com>
2024-09-15 20:49:38 +0900

    feat: 옷장 디자인 추가
    Change-Id: I15ad2f81fbf3adf0d0abef9ca8864f44e5726c45

---
+++ client/src/app/[locale]/room/_components/character.jsx
@@ -0,0 +1,17 @@
+export default function Character() {
+  return (
+    <div className="relative flex items-center justify-center z-10">
+      <div className="w-full px-4">
+        <div className="grid grid-cols-3">
+          {[...Array(15)].map((_, index) => (
+            <div key={index} className="relative pb-[73.5%]">
+              <div className="border-2 border-green-500 absolute inset-0 flex items-center justify-center text-white font-bold text-lg">
+                {index + 1}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}

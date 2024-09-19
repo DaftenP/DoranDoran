151844d74f1606abf597807c0d4eebd4551c083b
KJH <ever618@naver.com>
2024-09-19 16:46:02 +0900

    design: 랭킹 페이지 All 부분 구현
    Change-Id: I0ba0b4cb88068cbcb5322b1062302f44b8c1cf66

---
+++ client/src/app/[locale]/ranking/_components/ranklist-all.jsx
@@ -0,0 +1,29 @@
+'use-client';
+
+import Image from "next/image";
+import Bronze from "@/public/rank/bronze.png";
+import Silver from "@/public/rank/silver.png";
+import Gold from "@/public/rank/gold.png";
+import Platinum from "@/public/rank/platinum.png";
+import Diamond from "@/public/rank/diamond.png";
+import Bird2 from "@/public/shop-bird/bird (2).png";
+
+export default function RankListAll({userRank, userChar, userId, userTier, userXP, borderColor}) {
+  return (
+    <div className="relative flex items-center bg-white px-[4%] py-[2%] font-normal font-['Itim'] rounded-lg shadow-md my-[2%]"  style={{ border: `2px solid ${borderColor}` }}>
+      <span className="font-bold text-gray-600">{userRank}</span>
+      <Image
+        src={Bird2}
+        alt="bird2"
+        className="w-[auto] h-[80%] left-[12%] absolute"
+      />
+      <span className="left-[25%] absolute text-purple-500 font-medium">{userId}</span>
+      <Image
+        src={Diamond}
+        alt="diamond"
+        className="w-[auto] h-[80%] left-[60%] absolute"
+      />
+      <span className="left-[80%] absolute text-blue-600 font-semibold">{userXP}</span>
+    </div>
+  );
+}

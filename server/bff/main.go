28f46700d7657de36bf77af975dcbf4025741b0e
김상범 <picel@picel.net>
2024-09-26 11:39:44 +0900

    Feat: add User delete api
    Change-Id: If24a7ac7aa304bf1fe2300018b786373be31d8f0

--- server/bff/main.go
+++ server/bff/main.go
@@ -12,6 +12,8 @@ func main() {
 	http.HandleFunc("/api/v1/bff/regist", controller.Regist)
 	http.HandleFunc("/api/v1/bff/login", controller.Login)
 
+	http.HandleFunc("/api/v1/bff/my-page/user", controller.DeleteUserController)
+
 	http.Handle("/api/v1/bff/admin/quiz", middleware.JWTMiddleware(http.HandlerFunc(controller.GenerateQuizController)))
 
 	http.Handle("/api/v1/bff/talk/send", middleware.JWTMiddleware(http.HandlerFunc(controller.SendController)))

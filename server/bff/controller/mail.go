eaff10f8aa1988c332b46bb971e9c4575695b3c7
김상범 <picel@picel.net>
2024-09-26 12:44:53 +0900

    Feat: add mail/regist api
    Change-Id: Ice850e5f968c01098577f8275f6063a83c033e4d

---
+++ server/bff/controller/mail.go
@@ -0,0 +1,19 @@
+package controller
+
+import (
+	"net/http"
+
+	"com.doran.bff/service"
+	"com.doran.bff/util"
+)
+
+// POST /api/v1/bff/mail/regist
+func RegistMailController(w http.ResponseWriter, r *http.Request) {
+	email := r.URL.Query().Get("email")
+	if email == "" {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	util.ForwardRequest(w, r, "POST", service.UserUrl+"/api/v1/mail/regist?email="+email)
+}
--- server/bff/main.go
+++ server/bff/main.go
@@ -12,6 +12,8 @@ func main() {
 	http.HandleFunc("/api/v1/bff/regist", controller.Regist)
 	http.HandleFunc("/api/v1/bff/login", controller.Login)
 
+	http.HandleFunc("/api/v1/bff/mail/regist", controller.RegistMailController)
+
 	http.HandleFunc("/api/v1/bff/my-page/user", controller.DeleteUserController)
 
 	http.Handle("/api/v1/bff/admin/quiz", middleware.JWTMiddleware(http.HandlerFunc(controller.GenerateQuizController)))

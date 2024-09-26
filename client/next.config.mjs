438ab532ed916fadd1cd417e7d103ad30d2a7c8c
kdhn97 <ehdgus3726@naver.com>
2024-09-26 15:22:47 +0900

    modified: PWA 수정
    Change-Id: I503e88734e536787ea0d428ea7e51300f75140f7

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,4 @@
-import withPWA from 'next-pwa';
+import withPWAInit from "@ducanh2912/next-pwa";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
@@ -6,11 +6,11 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withPWAConfig = withPWA({
+const withPWAInitConfig = withPWAInit({
   dest: 'public', // PWA 관련 파일들이 생성될 디렉토리를 지정
   disable: process.env.NODE_ENV === 'development', // 개발 환경에서 PWA 비활성화
-  register: false, // 서비스 워커 자동 등록
+  register: true, // 서비스 워커 자동 등록
   skipWaiting: true, // PWA 업데이트 즉시 적용
 });
 
-export default withNextIntl(withPWAConfig(nextConfig));
+export default withNextIntl(withPWAInitConfig(nextConfig));

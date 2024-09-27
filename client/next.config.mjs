619f405c52935ef68fda7ca7f0fd5605450f3dc0
kdhn97 <ehdgus3726@naver.com>
2024-09-27 11:20:28 +0900

    modified: PWA 수정 - Weppack 설치
    Change-Id: Ie1cbf5b16b4fa9f5b142a1b74e9342bdc645388e

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -9,6 +9,7 @@ const nextConfig = {};
 const withPWAInitConfig = withPWAInit({
   dest: 'public', // PWA 관련 파일들이 생성될 디렉토리를 지정
   disable: process.env.NODE_ENV === 'development', // 개발 환경에서 PWA 비활성화
+  // disable: false, // PWA를 항상 활성화
   register: true, // 서비스 워커 자동 등록
   skipWaiting: true, // PWA 업데이트 즉시 적용
 });

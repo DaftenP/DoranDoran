eea4114ac7ef62c30c89b20a4de57371ed4b6fff
kdhn97 <ehdgus3726@naver.com>
2024-09-29 18:58:31 +0900

    feat: 회원가입 및 비밀번호 변경 수정
    Change-Id: I696c141b388b8db130f11179acc87a4b7e68bf93

--- client/src/components/modal/modal.jsx
+++ client/src/components/modal/modal.jsx
@@ -1,21 +1,20 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { useLocale, useTranslations, NextIntlClientProvider } from 'next-intl';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import Image from 'next/image'
 import ModalBird from '@/public/modal/modal-bird.webp'
 import ModalNight from '@/public/modal/modal-night.webp'
 
-export default function Modal({ handleYesClick, handleCloseModal, message }) {
+export default function Modal({ handleYesClick, handleCloseModal, message, background, buttonType }) {
   const [messages, setMessages] = useState(null);
-  const locale = useLocale(); // 예시로 en 사용, 동적 처리 가능
+  const locale = useLocale();
 
   useEffect(() => {
     async function loadMessages() {
       try {
         const loadedMessages = await import(`messages/${locale}.json`);
-        setMessages(loadedMessages.default); // 메시지 로드
+        setMessages(loadedMessages.default);
       } catch (error) {
         console.error(`Failed to load messages for locale: ${locale}`);
       }
@@ -24,35 +23,45 @@ export default function Modal({ handleYesClick, handleCloseModal, message }) {
   }, [locale]);
 
   if (!messages) {
-    return <div>Loading...</div>; // 메시지가 로드될 때까지 로딩 표시
+    return <div>Loading...</div>;
   }
 
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
-      <TranslatedModal handleYesClick={handleYesClick} handleCloseModal={handleCloseModal} message={message} />
+      <TranslatedModal 
+        handleYesClick={handleYesClick} 
+        handleCloseModal={handleCloseModal} 
+        message={message}
+        background={background}
+        buttonType={buttonType}
+      />
     </NextIntlClientProvider>
   );
 }
 
-function TranslatedModal({ handleYesClick, handleCloseModal, message }) {
+function TranslatedModal({ handleYesClick, handleCloseModal, message, background, buttonType }) {
   const t = useTranslations('index');
-
   return (
     <div className='fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center'>
       <div className="relative bg-white p-6 rounded-lg shadow-lg w-[70vw] h-[70vw] md:w-[50vw] md:h-[40vw] lg:w-[50vw] lg:h-[40vw] overflow-hidden">
         <div className='relative z-10 text-center text-xl md:text-2xl lg:text-4xl'>
-          {t(message.message)}
+          {t(message)}
         </div>
-        {message.background === 'bird' &&  (
-          <Image src={ModalBird} alt="background_bird" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-70 w-[40vw] h-auto md:w-[30vw] md:h-auto lg:w-[30vw] lg:h-auto z-0" />
+        {background === 'bird' &&  (
+          <Image src={ModalBird} alt="background_bird" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-70 w-[40vw] h-auto md:w-[30vw] md:h-auto lg:w-[30vw] lg:h-auto z-0" priority />
         )}
-        {message.background === 'night' &&  (
-          <Image src={ModalNight} alt="background_night" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full z-0" />
+        {background === 'night' &&  (
+          <Image src={ModalNight} alt="background_night" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full z-0" priority />
         )}
-        {message.buttonType === 1 && (
+        {buttonType === 1 && (
           <div className='absolute left-1/2 transform -translate-x-1/2 bottom-[5vw] w-full flex justify-around'>
             <button onClick={handleCloseModal} className="mt-4 p-2 bg-red-500 text-white rounded z-10 w-[20vw] md:w-[15vw] text-xl md:text-2xl lg:text-4xl">{t('no')}</button>
-            <button onClick={() => handleYesClick(message.buttonLink)} className=" mt-4 p-2 bg-red-500 text-white rounded z-10 w-[20vw] md:w-[15vw] text-xl md:text-2xl lg:text-4xl">{t('yes')}</button>
+            <button onClick={handleYesClick} className=" mt-4 p-2 bg-red-500 text-white rounded z-10 w-[20vw] md:w-[15vw] text-xl md:text-2xl lg:text-4xl">{t('yes')}</button>
+          </div>
+        )}
+        {buttonType === 2 && (
+          <div className='absolute left-1/2 transform -translate-x-1/2 bottom-[5vw] w-full flex justify-around'>
+            <button onClick={handleCloseModal} className="mt-4 p-2 bg-red-500 text-white rounded z-10 w-[20vw] md:w-[15vw] text-xl md:text-2xl lg:text-4xl">{t('check')}</button>
           </div>
         )}
       </div>

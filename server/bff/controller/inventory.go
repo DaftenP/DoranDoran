233f02b746a7773f8d50a083bebb226761fde2c7
김상범 <picel@picel.net>
2024-10-03 13:59:05 +0900

    Refactor: Update EquipItem API in inventory controller and service
    - fix data type of userId for msa from string to int
    
    Change-Id: Iffe2dc0d6e8931cef7cd2f58e4b663418a59c1db

--- server/bff/controller/inventory.go
+++ server/bff/controller/inventory.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"strings"
@@ -41,18 +42,62 @@ func GetItems(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-// PATCH /api/v1/bff/inventory/equip/{itemId}
+// PATCH /api/v1/bff/inventory/equip
 func EquipItem(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPatch {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// get itemId from path
-	itemId := r.URL.Path[len("/api/v1/bff/inventory/equip/"):]
-	if itemId == "" {
-		http.Error(w, "itemId is required", http.StatusBadRequest)
+	cookie, err := r.Cookie("refresh")
+	if err != nil {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
 		return
 	}
 
+	parts := strings.Split(cookie.Value, "%3A")
+	userId := parts[0]
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		http.Error(w, "Error parsing request body", http.StatusInternalServerError)
+		return
+	}
+
+	// get itemType and itemId from request body
+	itemType, ok := data["itemType"]
+	if !ok {
+		http.Error(w, "Invalid item type", http.StatusBadRequest)
+		return
+	}
+
+	itemId, ok := data["itemId"]
+	if !ok {
+		http.Error(w, "Invalid item ID", http.StatusBadRequest)
+		return
+	}
+
+	req, err := service.EquipItemService(userId, itemType.(string), itemId.(string))
+	if err != nil {
+		http.Error(w, "Error sending request", http.StatusInternalServerError)
+		return
+	}
+	defer req.Body.Close()
+
+	// forward response body to client
+	_, err = io.Copy(w, req.Body)
+	if err != nil {
+		http.Error(w, "Error forwarding response", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }

b1f9b34dff8522270c69bf6c2729168fb72158c6
kdhn97 <ehdgus3726@naver.com>
2024-10-03 13:27:33 +0900

    !HOTFIX: PWA 이미지 PNG 변환
    Change-Id: Ie242d924aa504e32c687dab0cf57d93a28ddbaeb

--- client/src/app/manifest.js
+++ client/src/app/manifest.js
@@ -10,28 +10,52 @@ export default function manifest() {
     description: "Korean Learning Platform",
     icons: [
       {
+        src: "/bird144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/bird192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/bird256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/bird512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
         src: "/bird144.webp",
         sizes: "144x144",
         type: "image/webp",
-        purpose: "any"
+        purpose: "any",
       },
       {
         src: "/bird192.webp",
         sizes: "192x192",
         type: "image/webp",
-        purpose: "maskable"
+        purpose: "maskable",
       },
       {
         src: "/bird256.webp",
         sizes: "256x256",
         type: "image/webp",
-        purpose: "any"
+        purpose: "any",
       },
       {
         src: "/bird512.webp",
         sizes: "512x512",
         type: "image/webp",
-        purpose: "any"
+        purpose: "any",
       },
     ],
   };

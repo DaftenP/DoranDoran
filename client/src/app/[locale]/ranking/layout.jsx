4b05dd3617f94abdb635c20647bdbc072bd6d444
there2989 <there2989@naver.com>
2024-10-04 17:23:00 +0900

    feat: layout 배경 변경 추가
    Change-Id: I8c54ecaa66b4ea6e3a439cbe51a88fa88bc44c55

--- client/src/app/[locale]/ranking/layout.jsx
+++ client/src/app/[locale]/ranking/layout.jsx
@@ -1,21 +1,46 @@
-import Image from "next/image";
-import BackgroundDay from "@/public/background/day.webp";
-import Bottom from '@/components/bottom/bottom';
-import Top from '@/components/top/top';
+'use client'
+
+import { useSelector } from "react-redux";
+import One from "@/public/background/day.webp";
+import Two from "@/public/background/launch.webp";
+import Three from "@/public/background/day-blue.webp";
+import Four from "@/public/background/night-blue.webp";
+import Five from "@/public/background/launch-blue.webp";
+import Top from "@/components/top/top";
+import Bottom from "@/components/bottom/bottom";
 
 export default function Layout({ children }) {
+  const userBackground = useSelector((state) => state.user.profile.background);
+
+  const renderBackground = () => {
+    switch (userBackground) {
+      case 1:
+        return One.src;
+      case 2:
+        return Two.src;
+      case 3:
+        return Three.src;
+      case 4:
+        return Four.src;
+      case 5:
+        return Five.src;
+      default:
+        return One.src; // 기본 배경 이미지
+    }
+  };
+
   return (
-    <div className="relative min-h-screen">
-      <Image
-        src={BackgroundDay}
-        alt="background-day"
-        fill
-        className="z-0"
-        priority
-      />
-      <Top className="relative z-2"/>
+    <div
+      className="relative min-h-screen"
+      style={{
+        backgroundImage: `url(${renderBackground()})`, // 배경 이미지로 설정
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Top className="relative z-2" />
       <div className="relative z-1">{children}</div>
-      <Bottom className="relative z-2"/>
+      <Bottom className="relative z-2" />
     </div>
   );
 }
--- client/src/store/user.js
+++ client/src/store/user.js
@@ -14,7 +14,6 @@ export const fetchUserData = createAsyncThunk(
         },
         withCredentials: true,
       });
-
       return response.data;
     } catch (error) {
       console.log(error)

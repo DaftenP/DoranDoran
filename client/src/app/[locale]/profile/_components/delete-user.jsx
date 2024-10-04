74ec425d96e3b1dce421310c29eebe7b85f0794a
kdhn97 <ehdgus3726@naver.com>
2024-10-04 17:41:28 +0900

    modified: 회원탈퇴 경고창
    Change-Id: Id39b3bf884797afd07cce6cf3dec4f7691d7b292

--- client/src/app/[locale]/profile/_components/delete-user.jsx
+++ client/src/app/[locale]/profile/_components/delete-user.jsx
@@ -4,6 +4,7 @@ import { useLocale, useTranslations, NextIntlClientProvider } from "next-intl";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import Modal from "@/components/modal/modal";
 
 export default function DeleteUser() {
   const [messages, setMessages] = useState(null);
@@ -34,32 +35,62 @@ function TranslatedDeleteUser() {
   const t = useTranslations("index");
   const router = useRouter();
   const locale = useLocale();
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState(null);
 
-  const handleDeleteUser = (e) => {
-    e.preventDefault();
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    axios
-      .delete(`${apiUrl}/my-page/user`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      })
-      .then(() => {
-        localStorage.removeItem("accessToken");
-        router.push(`/${locale}/`);
-      })
-      .catch((error) => {});
+  const handleOpenModal = (messageIndex) => {
+    setModalMessage(modalMessages[messageIndex]);
+    setIsOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
+
+  const modalMessages = [
+    {
+      message: "do-you-want-to-leave?",
+      background: "bird",
+      buttonLink: "profile/setting",
+      buttonType: 1,
+    },
+  ];
+
+  const handleYesClick = (buttonLink) => {
+    if (buttonLink === "profile/setting") {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      axios
+        .delete(`${apiUrl}/my-page/user`, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        })
+        .then(() => {
+          localStorage.removeItem("accessToken");
+          router.push(`/${locale}/`);
+        })
+        .catch((error) => {});
+    }
   };
 
   return (
     <>
       <button
         className="bg-red-500 rounded-xl py-1 px-3"
-        onClick={handleDeleteUser}
+        onClick={() => handleOpenModal(0)}
       >
         <p className="text-white text-xl md:text-3xl">{t("delete-user")}</p>
       </button>
+      {isOpenModal && (
+        <div>
+          <Modal
+            handleYesClick={handleYesClick}
+            handleCloseModal={handleCloseModal}
+            message={modalMessage}
+          />
+        </div>
+      )}
     </>
   );
 }

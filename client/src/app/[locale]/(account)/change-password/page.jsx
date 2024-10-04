286a645154219398463e23f95de9f28d87a85fa5
kdhn97 <ehdgus3726@naver.com>
2024-10-04 09:55:54 +0900

    modified: 임시 비밀번호 변경 수정
    Change-Id: I2dd508527462330eb79b2c449d67f5ac887d5919

--- client/src/app/[locale]/(account)/change-password/page.jsx
+++ client/src/app/[locale]/(account)/change-password/page.jsx
@@ -44,10 +44,15 @@ function TranslatedChange() {
   const locale = useLocale();
   const t = useTranslations("index");
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const [formData, setFormData] = useState({ email: "" });
-  const [touched, setTouched] = useState({ email: false });
-  const [modalMessage, setModalMessage] = useState("");  
+  const [formData, setFormData] = useState({ email: "", verificationCode: "" });
+  const [touched, setTouched] = useState({
+    email: false,
+    verificationCode: false,
+  });
+  const [modalMessage, setModalMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [isEmailVerified, setIsEmailVerified] = useState(false);
+  const [isEmailSent, setIsEmailSent] = useState(false);
 
   // 입력 변경 핸들러
   const signupChange = (e) => {
@@ -62,14 +67,17 @@ function TranslatedChange() {
   };
 
   // 모달 닫기 핸들러
-  const handleCloseModal = () => {setIsOpenModal(false)};
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+  };
 
   // 이메일 전송 핸들러
   const emailSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    axios.put(`${apiUrl}/mail/reset`, null, {
+    axios
+      .post(`${apiUrl}/mail/regist`, null, {
         params: { email: formData.email },
         headers: {
           "Content-Type": "application/json",
@@ -77,13 +85,98 @@ function TranslatedChange() {
       })
       .then((response) => {
         setIsLoading(false);
-        setModalMessage({ message: "email-sending-successful", background: "bird", buttonType: 2 });
+        setIsEmailSent(true);
+        setModalMessage({
+          message: "email-sending-successful",
+          background: "bird",
+          buttonType: 2,
+        });
       })
       .catch((error) => {
         setIsLoading(false);
-        setModalMessage({ message: "email-sending-failed", background: "bird", buttonType: 2 });
+        setIsEmailSent(false);
+        setModalMessage({
+          message: "email-sending-failed",
+          background: "bird",
+          buttonType: 2,
+        });
       })
-      .finally(() => {setIsOpenModal(true)});
+      .finally(() => {
+        setIsOpenModal(true);
+      });
+  };
+
+  // 임시 비밀번호 변경
+  const PasswordSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    axios
+      .put(`${apiUrl}/mail/reset`, null, {
+        params: { email: formData.email },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        setIsLoading(false);
+        setIsEmailSent(true);
+        setModalMessage({
+          message: "Passcode issued successfully",
+          background: "bird",
+          buttonType: 2,
+        });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setIsEmailSent(false);
+        setModalMessage({
+          message: "Failed to issue password",
+          background: "bird",
+          buttonType: 2,
+        });
+      })
+      .finally(() => {
+        setIsOpenModal(true);
+      });
+  };
+
+  // 인증 코드 확인 핸들러
+  const verifyCode = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    axios
+      .get(`${apiUrl}/mail/check`, {
+        params: {
+          email: formData.email,
+          userNumber: formData.verificationCode,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        setIsLoading(false);
+        setIsEmailVerified(true);
+        setModalMessage({
+          message: "verification-successful",
+          background: "bird",
+          buttonType: 2,
+        });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setIsEmailVerified(false);
+        setModalMessage({
+          message: "verification-failed",
+          background: "bird",
+          buttonType: 2,
+        });
+      })
+      .finally(() => {
+        setIsOpenModal(true);
+      });
   };
 
   // 입력 필드 기본 스타일
@@ -97,15 +190,22 @@ function TranslatedChange() {
       case "email":
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return `${inputStyle} ${
-          emailRegex.test(value) ? "border-2 border-green-500" : "border-2 border-red-500"
+          emailRegex.test(value)
+            ? "border-2 border-green-500"
+            : "border-2 border-red-500"
         }`;
-      default: return inputStyle}
+      default:
+        return inputStyle;
+    }
   };
 
   return (
     <>
-    {isLoading && <LoadingOverlay />}
-      <div className="w-screen h-screen flex flex-col items-center">
+      {isLoading && <LoadingOverlay />}
+      <form
+        onSubmit={PasswordSubmit}
+        className="w-screen h-screen flex flex-col items-center"
+      >
         {/* 제목 */}
         <div className="mt-14 mb-7 text-center">
           <p className="text-2xl md:text-5xl mb-2">{t("change-password")}</p>
@@ -113,7 +213,7 @@ function TranslatedChange() {
         </div>
         <div className="w-[75%]">
           {/* 이메일 입력 필드 */}
-          <div className="relative">
+          <div className="relative mb-3">
             <div className="flex">
               <p className="text-red-500 md:text-3xl mr-1">*</p>
               <p className="md:text-3xl">{t("e-mail")}</p>
@@ -131,15 +231,45 @@ function TranslatedChange() {
               type="button"
               onClick={emailSubmit}
               className="absolute top-1/2 right-5 md:text-3xl bg-gradient-to-r from-[#DBB4F3] to-[#FFC0B1] text-transparent bg-clip-text"
+              disabled={!formData.email || isEmailSent}
             >
               {t("send")}
+            </button>
+          </div>
+
+          {/* 인증 코드 입력 필드 */}
+          <div className="relative">
+            <div className="flex">
+              <p className="text-red-500 md:text-3xl mr-1">*</p>
+              <p className="md:text-3xl">{t("verification-code")}</p>
+            </div>
+            <input
+              type="text"
+              name="verificationCode"
+              value={formData.verificationCode}
+              onChange={signupChange}
+              onBlur={handleBlur}
+              placeholder="123456"
+              className={getInputClass(
+                "verificationCode",
+                formData.verificationCode
+              )}
+              required
+            />
+            <button
+              type="button"
+              onClick={verifyCode}
+              className="absolute top-1/2 right-5 md:text-3xl bg-gradient-to-r from-[#DBB4F3] to-[#FFC0B1] text-transparent bg-clip-text"
+              disabled={!formData.verificationCode || isEmailVerified}
+            >
+              {t("confirm")}
             </button>
           </div>
 
           {/* 취소 및 확인 버튼 */}
           <div className="flex justify-center">
             <Button text={t("cancel")} href={`/${locale}`} />
-            <Button text={t("ok")} href={`/${locale}`} />
+            <Button text={t("ok")} type="submit" />
           </div>
         </div>
 
@@ -156,7 +286,7 @@ function TranslatedChange() {
           className="w-[28%] absolute top-[60%] left-[15%]"
           priority
         />
-      </div>
+      </form>
 
       {/* 모달 컴포넌트 */}
       {isOpenModal && (

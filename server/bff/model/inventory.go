645934d3e9c68c9577a93887d11cff02eaa7763d
김상범 <picel@picel.net>
2024-10-07 16:28:38 +0900

    Hotfix: change param name of equip model
    Change-Id: I1511228ee60d2e5f9c5812752c3db9be38c96845

--- server/bff/model/inventory.go
+++ server/bff/model/inventory.go
@@ -1,6 +1,6 @@
 package model
 
 type EquipRequestFromClient struct {
-	ItemId   int `json:"item_id"`
-	ItemType int `json:"item_type"`
+	ItemId   int `json:"itemId"`
+	ItemType int `json:"itemType"`
 }

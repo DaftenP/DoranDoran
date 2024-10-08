fa25006458e3bb8088c9ff5c0f9c6b18e6a61c39
kdhn97 <ehdgus3726@naver.com>
2024-10-08 09:23:19 +0900

    !HOTFIX: 언어 설정 오류 해결
    Change-Id: I74dfde81b1af1a9883e5bd36a470845f4c0126d3

--- client/src/app/[locale]/(account)/_components/language.jsx
+++ client/src/app/[locale]/(account)/_components/language.jsx
@@ -6,14 +6,9 @@ import { useRouter, usePathname } from "next/navigation";
 
 export default function Language() {
   const [messages, setMessages] = useState(null);
-  const [locale, setLocale] = useState(useLocale());
+  const locale = useLocale();
 
   useEffect(() => {
-    const savedLocale = localStorage.getItem('selectedLocale');
-    if (savedLocale) {
-      setLocale(savedLocale);
-    }
-
     async function loadMessages() {
       try {
         const loadedMessages = await import(`messages/${locale}.json`);
@@ -25,13 +20,14 @@ export default function Language() {
 
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
-      <TranslatedLanguage setLocale={setLocale} />
+      <TranslatedLanguage />
     </NextIntlClientProvider>
   );
 }
 
-function TranslatedLanguage({ setLocale }) {
+function TranslatedLanguage() {
   const t = useTranslations("index");
+  const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
@@ -45,26 +41,19 @@ function TranslatedLanguage({ setLocale }) {
     { code: "ru", name: "russian" },
   ];
 
-  // localStorage에서 저장된 언어 코드를 가져오거나, 없으면 현재 locale 사용
-  const savedLocale = typeof window !== 'undefined' ? localStorage.getItem('selectedLocale') : null;
-  const initialLocale = savedLocale || useLocale();
-
   // 현재 locale에 해당하는 언어를 찾아 초기값 설정
   const currentLanguage =
-    languages.find((lang) => lang.code === initialLocale)?.name || "english";
+    languages.find((lang) => lang.code === locale)?.name || "english";
   const [selectedLanguage, setSelectedLanguage] = useState(currentLanguage);
 
   // 언어 선택 드롭다운 토글 함수
   const toggleDropdown = () => setIsOpen(!isOpen);
-  
   // 언어 선택 시 실행되는 함수
   const selectLanguage = (lang) => {
     setSelectedLanguage(lang.name);
     setIsOpen(false);
     const newPathname = pathname.replace(/^\/[a-z]{2}/, `/${lang.code}`);
     router.push(newPathname);
-    localStorage.setItem('selectedLocale', lang.code);
-    setLocale(lang.code);
   };
 
   return (

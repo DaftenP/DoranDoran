64d0411ecd8d659b9e08016ab0eb5ed111d915f4
김상범 <picel@picel.net>
2024-10-10 00:08:31 +0900

    refactor: 재생 버튼 디자인 개선, 정답 표시 (추후 백엔드 수정 예정)
    Change-Id: I347e177981b348027dfd97f6d441cab6585b1b78

--- client/src/app/[locale]/study/_components/quiz-detail.jsx
+++ client/src/app/[locale]/study/_components/quiz-detail.jsx
@@ -1,24 +1,24 @@
-'use client';
+"use client";
 
 import Image from "next/image";
-import { useLocale, useTranslations, NextIntlClientProvider } from 'next-intl';
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchStageDetail } from '@/store/quiz';
-import QuizTitle from '@/app/[locale]/study/_components/quiz-title'
-import QuizContent from '@/app/[locale]/study/_components/quiz-content';
+import { useLocale, useTranslations, NextIntlClientProvider } from "next-intl";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchStageDetail } from "@/store/quiz";
+import QuizTitle from "@/app/[locale]/study/_components/quiz-title";
+import QuizContent from "@/app/[locale]/study/_components/quiz-content";
 import Play from "@/public/icon/play1.webp";
 import Pause from "@/public/icon/play2.webp";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
-export default function Quiz({type, index}) {
+export default function Quiz({ type, index }) {
   const dispatch = useDispatch();
   const [messages, setMessages] = useState(null);
   const locale = useLocale();
 
   useEffect(() => {
     // if (index) {
-      dispatch(fetchStageDetail(index+1)); // Fetch the stage detail using the ID
+    dispatch(fetchStageDetail(index + 1)); // Fetch the stage detail using the ID
     // }
   }, [dispatch, index]);
 
@@ -40,16 +40,16 @@ export default function Quiz({type, index}) {
 
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
-      <TranslatedQuiz locale={locale} type={type} index={index}/>
+      <TranslatedQuiz locale={locale} type={type} index={index} />
     </NextIntlClientProvider>
   );
 }
 
-function TranslatedQuiz({locale, type, index}) {
-  const t = useTranslations('index');
+function TranslatedQuiz({ locale, type, index }) {
+  const t = useTranslations("index");
   const stageData = useSelector((state) => state.quiz.stage.data);
   const quizList = useSelector((state) => state.quiz.stageDetail.data);
-  
+
   const stageOrder = stageData[index]?.order;
   const quizType = quizList[0]?.quizType;
   const totalQuizzes = quizList.length;
@@ -58,6 +58,7 @@ function TranslatedQuiz({locale, type, index}) {
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [audio, setAudio] = useState(null);
+  const [answer, setAnswer] = useState(null);
 
   useEffect(() => {
     if (quizVoiceUrl) {
@@ -81,43 +82,67 @@ function TranslatedQuiz({locale, type, index}) {
     if (!audio) return;
 
     const handleEnded = () => setIsPlaying(false);
-    audio.addEventListener('ended', handleEnded);
+    audio.addEventListener("ended", handleEnded);
 
     return () => {
-      audio.removeEventListener('ended', handleEnded);
+      audio.removeEventListener("ended", handleEnded);
       audio.pause();
     };
   }, [audio]);
 
   useEffect(() => {
     if (!quizList.length) {
-      router.push(`/${locale}/study`)
+      router.push(`/${locale}/study`);
+    }
+  }, [quizList]);
+
+  useEffect(() => {
+    if (quizList.length) {
+      setAnswer(quizList[0]?.quizAnswer);
     }
   }, [quizList]);
 
   return (
-    <div className='relative h-[80vh]'>
-      <div className='mb-[10vh]'>
-        <div className='text-center text-3xl md:text-4xl lg:text-6xl'>
-          Stage-{stageOrder}
-        </div>
-        <div className='text-center text-xl md:text-2xl lg:text-4xl'>
+    <div className="relative h-[80vh]">
+      <div className="mb-[10vh]">
+        <div className="text-center text-3xl md:text-4xl lg:text-6xl">Stage-{stageOrder}</div>
+        <div className="text-center text-xl md:text-2xl lg:text-4xl">
           {/* {`[${1}/${totalQuizzes}]`}  */}
-          남은 퀴즈 수 :  {totalQuizzes}
+          남은 퀴즈 수 : {totalQuizzes}
         </div>
-        <QuizTitle type={type} index={index}/>
+        <QuizTitle type={type} index={index} />
       </div>
       {quizType === 5001 && (
-        <button onClick={toggleAudio} className='absolute z-10 top-[82%] left-[50%] transform -translate-x-1/2 flex-col flex justify-center items-center w-[30vw] h-[10vh]'>
-          <Image
-            src={isPlaying ? Pause : Play}
-            alt="play"
-            className="w-[30%] h-auto" // CSS에서 비율을 맞추기 위해 w와 h를 설정
-          />
+        <button
+          onClick={toggleAudio}
+          className="absolute z-10 top-[82%] left-[50%] transform -translate-x-1/2 flex-col flex justify-center items-center w-[30vw] h-[10vh]">
+          <div
+            className="w-[50vw] flex justify-between items-center"
+            style={{
+              backgroundColor: "#23cccc",
+              padding: "2vw 5vw 2vw 2vw",
+              borderRadius: "50px",
+            }}>
+            <div
+              style={{
+                backgroundColor: "#FFFFF0",
+                width: "13vw",
+                height: "13vw",
+                padding: "3vw",
+                borderRadius: "50%",
+              }}>
+              <Image
+                src={isPlaying ? Pause : Play}
+                alt="play"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </div>
+            <div className="ml-5 text-xl md:text-2xl lg:text-3xl">{answer}</div>
+          </div>
         </button>
       )}
-      <div className='absolute top-[35%] w-[80%] h-[75%] left-1/2 transform -translate-x-1/2'>
-        <QuizContent type={type} index={index}/>
+      <div className="absolute top-[35%] w-[80%] h-[75%] left-1/2 transform -translate-x-1/2">
+        <QuizContent type={type} index={index} />
       </div>
     </div>
   );

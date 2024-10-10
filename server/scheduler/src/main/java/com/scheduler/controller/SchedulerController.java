f34cb1089f348d80a6d0d2dee130666c3eaa283f
김세진 <tpwls1355@naver.com>
2024-10-10 21:09:44 +0900

    !HOTFIX: 스케줄러 API 지난주 리그 갱신
    Change-Id: I63b264f4877511461c0d40c64e6163c05a69502a

--- server/scheduler/src/main/java/com/scheduler/controller/SchedulerController.java
+++ server/scheduler/src/main/java/com/scheduler/controller/SchedulerController.java
@@ -3,10 +3,12 @@ package com.scheduler.controller;
 import com.scheduler.common.ResponseDto;
 import com.scheduler.common.exception.StatusCode;
 import com.scheduler.job.LeaderBoardBatch;
+import com.scheduler.service.LeaderBoardService.LeaderBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SchedulerController {
     private final LeaderBoardBatch leaderBoardBatch;
+    private final LeaderBoardService leaderboardService;
 
     @GetMapping("/league/settlement")
     public ResponseEntity<ResponseDto> leagueSettlement() {
@@ -21,9 +24,15 @@ public class SchedulerController {
         return ResponseDto.response(StatusCode.SUCCESS, null);
     }
 
-    @GetMapping("/leaderboard/update")
-    public ResponseEntity<ResponseDto> updateLeaderBoard() {
-        leaderBoardBatch.updateLeaderBoard();
+    @GetMapping("/leaderboard/current-update")
+    public ResponseEntity<ResponseDto> updateCurrentLeaderBoard() {
+        leaderboardService.updateCurrentLeaderBoard();
+        return ResponseDto.response(StatusCode.SUCCESS, null);
+    }
+
+    @GetMapping("/leaderboard/last-update")
+    public ResponseEntity<ResponseDto> updateLastLeaderBoard() {
+        leaderboardService.updateLastLeaderBoard();
         return ResponseDto.response(StatusCode.SUCCESS, null);
     }
 }
